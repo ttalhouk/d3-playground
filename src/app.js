bba2cdef956e796e756f3f4e00c12a16a0e81f19
Talal Talhouk <talal@sbcglobal.net>
2016-12-13 14:29:05 -0800

    add examples from accessing file data

--- src/app.js
+++ src/app.js
@@ -35,10 +35,39 @@
 
 // Ordinal Scales
 
-var ordinalScale = d3.scaleOrdinal()
-  .domain(["poor", "good", "great"])
-  .range(["red", "white", "green"])
+// var ordinalScale = d3.scaleOrdinal()
+//   .domain(["poor", "good", "great"])
+//   .range(["red", "white", "green"])
+//
+// console.log(ordinalScale("good"));
+// // domain is mapped to the range poor >> red, good >> white, great >> green
+// // this is based on position in the array
 
-console.log(ordinalScale("good"));
-// domain is mapped to the range poor >> red, good >> white, great >> green
-// this is based on position in the array
+// Load and Inspect data
+
+// CSV parsing
+d3.csv('data/data.csv', function(data){
+  console.log(data);
+})
+
+d3.tsv('data/data.tsv', function(data){
+  console.log(data);
+})
+
+
+d3.json('data/data.json', function (data) {
+  var extent = d3.extent(data, function (d) {
+    return d.age;
+  });
+  console.log(extent);
+
+  var scale = d3.scaleLinear()
+    .domain(extent)
+    .range([0, 600]);
+  console.log(scale(37));
+
+  var ages = d3.set(data, function (d) {
+    return d.age;
+  });
+  console.log(ages.values());
+})
