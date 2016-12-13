6792614307d5255dd183ce651e061e16f2fd3ffc
Talal Talhouk <talal@sbcglobal.net>
2016-12-13 14:09:50 -0800

    add quantize scale example

--- src/app.js
+++ src/app.js
@@ -21,3 +21,13 @@
 //   .range([0,100])
 //
 // console.log(timeScale(2016, 4, 15));
+
+// Quantize Scales
+var quantizeScale = d3.scaleQuantize()
+  .domain([0, 100])
+  .range(["red", "green"])
+
+console.log(quantizeScale(50));
+// domain is broken into the amount of range values so 0-49 is red, 50-100 is green
+
+console.log(quantizeScale.invertExtent("green")) 
