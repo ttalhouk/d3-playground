c4cdb4128d2c5e91207b0ab3f544bd83e575ef7c
Talal Talhouk <talal@sbcglobal.net>
2016-12-13 21:31:37 -0800

    add example of bar chart

--- src/app.js
+++ src/app.js
@@ -368,43 +368,43 @@
 
 // View Box responsiveness
 
-var margin = { top: 10, right: 20, bottom: 30, left: 30 };
-var width = 400 - margin.left - margin.right;
-var height = 600 - margin.top - margin.bottom;
-
-var svg = d3.select('.chart')
-  .append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
-    // pass svg to responsivefy
-    .call(responsivefy)
-  .append('g')
-    .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
-
-svg.append('rect')
-  .attr('width', width)
-  .attr('height', height)
-  .style('fill', 'lightblue')
-  .style('stroke', 'green');
-
-var yScale = d3.scaleLinear()
-  .domain([0, 100])
-  .range([height, 0]);
-var yAxis = d3.axisLeft(yScale);
-svg.call(yAxis);
-
-var xScale = d3.scaleTime()
-  .domain([new Date(2016, 0, 1, 6), new Date(2016, 0, 1, 9)])
-  .range([0, width]);
-
-var xAxis = d3.axisBottom(xScale)
-  .ticks(5)
-  .tickSize(10)
-  .tickPadding(5);
-svg
-  .append('g')
-    .attr('transform', `translate(0, ${height})`)
-  .call(xAxis);
+// var margin = { top: 10, right: 20, bottom: 30, left: 30 };
+// var width = 400 - margin.left - margin.right;
+// var height = 600 - margin.top - margin.bottom;
+//
+// var svg = d3.select('.chart')
+//   .append('svg')
+//     .attr('width', width + margin.left + margin.right)
+//     .attr('height', height + margin.top + margin.bottom)
+//     // pass svg to responsivefy
+//     .call(responsivefy)
+//   .append('g')
+//     .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+//
+// svg.append('rect')
+//   .attr('width', width)
+//   .attr('height', height)
+//   .style('fill', 'lightblue')
+//   .style('stroke', 'green');
+//
+// var yScale = d3.scaleLinear()
+//   .domain([0, 100])
+//   .range([height, 0]);
+// var yAxis = d3.axisLeft(yScale);
+// svg.call(yAxis);
+//
+// var xScale = d3.scaleTime()
+//   .domain([new Date(2016, 0, 1, 6), new Date(2016, 0, 1, 9)])
+//   .range([0, width]);
+//
+// var xAxis = d3.axisBottom(xScale)
+//   .ticks(5)
+//   .tickSize(10)
+//   .tickPadding(5);
+// svg
+//   .append('g')
+//     .attr('transform', `translate(0, ${height})`)
+//   .call(xAxis);
 
 function responsivefy(svg) {
   // get container + svg aspect ratio
@@ -432,3 +432,66 @@ function responsivefy(svg) {
       svg.attr("height", Math.round(targetWidth / aspect));
   }
 }
+
+// building a bar chart
+
+var margin = { top: 10, right: 20, bottom: 60, left: 30 };
+var width = 400 - margin.left - margin.right;
+var height = 565 - margin.top - margin.bottom;
+
+var svg = d3.select('.chart')
+  .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .call(responsivefy)
+  .append('g')
+    .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+
+var data = [
+  {score: 63, subject: 'Mathematics'},
+  {score: 82, subject: 'Geography'},
+  {score: 74, subject: 'Spelling'},
+  {score: 97, subject: 'Reading'},
+  {score: 52, subject: 'Science'},
+  {score: 74, subject: 'Chemistry'},
+  {score: 97, subject: 'Physics'},
+  {score: 52, subject: 'ASL'}
+];
+
+var yScale = d3.scaleLinear()
+  .domain([0, 100])
+  .range([height, 0]);
+var yAxis = d3.axisLeft(yScale);
+svg.call(yAxis);
+
+// Band scale used with bar charts
+var xScale = d3.scaleBand()
+  // space between bars (in a percentage of a bar)
+  .padding(0.2)
+  // supply domain with an array of the data subjects using map
+  .domain(data.map(d => d.subject))
+  .range([0, width]);
+
+var xAxis = d3.axisBottom(xScale)
+  .ticks(5)
+  .tickSize(10)
+  .tickPadding(5);
+svg
+  .append('g')
+    .attr('transform', `translate(0, ${height})`)
+  .call(xAxis)
+  .selectAll('text')
+  .style('text-anchor', 'end')
+  // rotates text to 45deg
+  .attr('transform', 'rotate(-45)');
+
+// creates rectangles for each data point
+svg.selectAll('rect')
+  .data(data)
+  .enter()
+  .append('rect')
+  .attr('x', d => xScale(d.subject))
+  .attr('y', d => yScale(d.score))
+  // sets widths of bands based on data supplied to xScale
+  .attr('width', d => xScale.bandwidth())
+  .attr('height', d => height - yScale(d.score));
